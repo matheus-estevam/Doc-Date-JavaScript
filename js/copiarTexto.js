9c2bfe461438639212a65b340ad072fb58e2206c
Sidraque <sidraqueagostinho150@gmail.com>
2023-12-05 10:48:53 -0300

    Update - Date Construtor

--- js/copiarTexto.js
+++ js/copiarTexto.js
@@ -1,4 +1,17 @@
+function copiarTexto(idTextarea) {
+    var textarea = document.querySelector(idTextarea);
 
-var clipboard = new ClipboardJS('.btn');
+    textarea.select();
+    document.execCommand('copy');
+}
 
+function executarCodigo(idTextarea) {
+    var textarea = document.querySelector(idTextarea);
+    var codigo = textarea.value;
 
+    try {
+        eval(codigo);
+    } catch (error) {
+        console.error('Erro ao executar o código:', error);
+    }
+}
