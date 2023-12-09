373302ea601cf22b3a0c69dd5c7d088b7050d29a
Sidraque <sidraqueagostinho150@gmail.com>
2023-12-08 23:24:29 -0300

    update

--- js/script.js
+++ js/script.js
@@ -6,3 +6,34 @@ $(document).ready(function () {
 });
 
 hljs.highlightAll();
+
+function copiarTexto(idTextarea) {
+    var textarea = document.querySelector(idTextarea);
+
+    textarea.select();
+    document.execCommand('copy');
+}
+
+function executarCodigo(idTextarea, idResultado) {
+    var textarea = document.querySelector(idTextarea);
+    var resultadoElement = document.querySelector(idResultado);
+    var codigo = textarea.value;
+
+    try {
+        // Limpe o conteúdo anterior antes de executar o novo código
+        resultadoElement.innerText = '';
+
+        // Sobrescreva a função console.log para capturar a saída
+        var resultado = '';
+        console.log = function (mensagem) {
+            resultado += mensagem + '\n';
+        };
+
+        eval(codigo);
+
+        // Exiba o resultado na própria página
+        resultadoElement.innerText = resultado;
+    } catch (error) {
+        console.error('Erro ao executar o código:', error);
+    }
+}
